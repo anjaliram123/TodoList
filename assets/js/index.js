528f0d63aa3b7842591962b18c2bbac63500493d
anjali <anjaliachalla@gmail.com>
2019-03-27 20:28:14 -0500

    Created a new to-do list using jquery

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+
+//check off 
+
+$('ul').on('click', 'li', function(){
+	$(this).toggleClass('completed');
+});
+
+//click on X to delete event
+
+$('ul').on('click', 'span', function(event) {
+
+$(this).parent().fadeOut(500, function(){
+      $(this).remove();
+});
+event.stopPropagation();
+});
+
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13) {
+      //getting new todo from input
+      var newtodo = $(this).val();  
+      $(this).val("");     
+      $('ul').append("<li><span>X</span> " + newtodo + "</li");    
+}
+});
